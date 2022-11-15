b833650ff8fcfbc6223d63d4798394ba0a1ac89b
harshabakku <sriharshabakku@gmail.com>
2022-11-15 21:15:34 +0530

    handleCORSRequest code in backend, fix componentDidMount loading twice issue, fetch royaltyRespectersData from backend api and try/display on frontend

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -1,7 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
+import ReactTable from "react-table-6";
+import axios from "axios";
 
-function App() {
+
+const getRoyaltyRespecters = async (collectionSymbol) => {
+  try {
+      const response = await axios.get(
+          `http://localhost:5000/api/service/royaltyRespecters?${collectionSymbol?"collectionSymbol="+collectionSymbol:""}`,
+      );
+      
+       console.log(response.data)
+      return response.data;
+  } catch (err) {
+      console.log("Error Fetching royalty respecters for collection ", collectionSymbol);
+  }
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      respectersData : [],
+      isLoading: true,
+      rowPerPage: 100,
+      pageNumber: 0,
+      pageSize: 0,
+    };
+  }
+
+
+
+componentDidMount = async () => {
+    
+  const collectionSymbol = undefined 
+  console.log("fetching royalty respecters for collection  .."+ collectionSymbol);
+  const respectersData  = await getRoyaltyRespecters(collectionSymbol);
+
+  this.setState({
+    isLoading: false,
+    respectersData: respectersData    
+  });
+
+};
+
+render() {
+
+  let { pageNumber, pageSize, rowPerPage, respectersData, isLoading } = this.state;
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +64,41 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing : Learn React
         </a>
       </header>
+
+      <ReactTable
+            data={respectersData}
+            columns={[
+                {
+                    Header: "Buyer",
+                    accessor: "buyer_address",
+                },
+                {
+                    Header: "Seller",
+                    accessor: "seller_address",
+                },
+            ]}
+            loading={isLoading}
+            loadingText={"Fetching Royalty Respecters for collection..."}
+            minRows={
+                respectersData?.length
+            }
+            manual={true}
+            manualPagination={true}
+            page={pageNumber}
+            pages={pageSize}
+            pageSize={rowPerPage}
+            defaultPageSize={rowPerPage}
+            pageSizeOptions={[100, 500, 1000]}
+            onPageChange={this.onPageChange}
+            onPageSizeChange={this.onRowPerPageChange}
+      />
     </div>
   );
+}
+
 }
 
 export default App;
