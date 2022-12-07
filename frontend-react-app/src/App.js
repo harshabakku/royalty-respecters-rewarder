1d835b484b4869867ff32da5424e22c903d0982d
harshabakku <sriharshabakku@gmail.com>
2022-12-07 14:16:55 +0530

    send raffle NFT transaction successful now

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -6,6 +6,14 @@ import { Button } from "reactstrap";
 import { Container, Row, Col } from 'react-bootstrap';
 
 import axios from "axios";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from  "@solana/web3.js";
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo, setAuthority, transfer } from  "@solana/spl-token";
+import * as buffer from "buffer";
+window.Buffer = buffer.Buffer;
+
+//recieves NFT
+const toWalletPublicKeyString = "YXAe6cRwn3UczVNpFWgr5vadupVcqT9Nq8qzvmmaMiX";
+
 
 const getProvider = () => {
   if ('phantom' in window) {
@@ -102,15 +110,81 @@ disconnectPhantomWallet =  async() =>{
 
 sendRaffleNftTransaction =  async() =>{
  
-  const provider = getProvider(); // see "Detecting the Provider"
-  const network = "https://api.devnet.solana.com"; //bring into config file
-  const connection = new Connection(network);
-  const transaction = new Transaction();
-  const { signature } = await provider.signAndSendTransaction(transaction);
-  console.log("sendRffleNftTransaction signature: "+ signature)
-  const status = await connection.getSignatureStatus(signature);
+  // Connect to cluster
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-  console.log("signature status" + status)
+  // Generate a new wallet keypair and airdrop SOL
+  const fromWallet = Keypair.generate();
+  const fromAirdropSignature = await connection.requestAirdrop(
+    fromWallet.publicKey,
+    LAMPORTS_PER_SOL
+  );
+
+  // Wait for airdrop confirmation
+  await connection.confirmTransaction(fromAirdropSignature);
+
+  // Create a new token 
+  const mint = await createMint(
+    connection, 
+    fromWallet,            // Payer of the transaction
+    fromWallet.publicKey,  // Account that will control the minting 
+    null,                  // Account that will control the freezing of the token 
+    0                      // Location of the decimal place 
+  );
+
+  // Get the token account of the fromWallet Solana address. If it does not exist, create it.
+  const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    fromWallet,
+    mint,
+    fromWallet.publicKey
+  );
+
+  // Generate a new wallet to receive the newly minted token
+  // const toWallet = Keypair.generate();
+ 
+  const toWalletPublicKey = new PublicKey(toWalletPublicKeyString);
+  // Get the token account of the toWallet Solana address. If it does not exist, create it.
+  const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    fromWallet,
+    mint,
+    toWalletPublicKey
+    // toWallet.publicKey
+
+  );
+
+  // Minting 1 new token to the "fromTokenAccount" account we just returned/created.
+  let signature = await mintTo(
+    connection,
+    fromWallet,               // Payer of the transaction fees 
+    mint,                     // Mint for the account 
+    fromTokenAccount.address, // Address of the account to mint to 
+    fromWallet.publicKey,     // Minting authority
+    1                         // Amount to mint 
+  );
+
+  console.log("NFT Mint SIGNATURE", signature);
+
+  await setAuthority(
+    connection,
+    fromWallet,            // Payer of the transaction fees
+    mint,                  // Account 
+    fromWallet.publicKey,  // Current authority 
+    0,                     // Authority type: "0" represents Mint Tokens 
+    null                   // Setting the new Authority to null
+  );
+
+  signature = await transfer(
+    connection,
+    fromWallet,               // Payer of the transaction fees 
+    fromTokenAccount.address, // Source account 
+    toTokenAccount.address,   // Destination account 
+    fromWallet.publicKey,     // Owner of the source account 
+    1                         // Number of tokens to transfer 
+  );
+
+  console.log("Transfer SIGNATURE", signature);
 
 }
 
@@ -129,6 +203,8 @@ computeRaffleWinner = async () => {
   await this.setState({
     raffleWinner : royaltyRespecters[randIndex].buyer_address
   })
+  
+  await this.sendRaffleNftTransaction()
 
 }
 
